094dd6d2226333f1551b5c41f9f7d22866c63103
Jonas Oscarsson <jonas.osc@rsson.nu>
2012-04-22 16:33:42 +0200

    Started implementing dep build of telldus.

--- build.sh
+++ build.sh
@@ -20,9 +20,23 @@ function build_owfs() {
 	sudo make install
 }
 
+function build_telldus() {
+	echo "===== telldus build started ====="
+	sudo apt-get install libftdi1 libconfuse0 libconfuse-dev libftdi-dev
+	echo -n "Untaring: "
+	pushd source > /dev/null
+	tar -xzf ../tar/telldus*
+	pushd telldus-core-2.1.1 > /dev/null
+	echo "done"
+	cmake . -DCMAKE_INSTALL_PREFIX=/opt/telldus
+	make
+	sudo make install
+}
+
 # Change to base dir and create source dir
 pushd $BASE > /dev/null
 mkdir -p source
 
 # Control what is built
-build_owfs
+#build_owfs
+build_telldus
